ea0bb6a70d584ca554cb3f4e4c2d499853ade704
Allison Budziak Wu <112855952+adbwu@users.noreply.github.com>
2023-02-09 07:06:57 -0500

    install MUI and searchbar toggle buttons

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,9 +1,21 @@
-import { useContext } from 'react'
+import React, { useContext } from 'react'
 import { SearchbarContext } from '../providers/SearchbarProvider';
+
+//MUI Imports
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 
 
 export default function Searchbar() {
   const { distFilter, updateKeys, updateDistFilter, geolocation, toggleGeolocation } = useContext(SearchbarContext);
+
+  // Material UI Consts
+  const [formats, setFormats] = React.useState(() => ['bold', 'italic']);
+  const [selected, setSelected] = React.useState(false);
+  const handleFormat = (event, newFormats) => {
+    setFormats(newFormats);
+  };
+
   
   return (
     <section style={ {color: "white", backgroundColor: "Green", width: "100%"} }>
@@ -18,27 +30,37 @@ export default function Searchbar() {
         value={distFilter.distance} 
         placeholder='Search Distance' 
       />   
-
-      <button
+    <ToggleButtonGroup
+      value={formats}
+      onChange={handleFormat}
+      aria-label="text formatting"
+    >
+      <ToggleButton
         onClick={() => updateKeys("key1")}>
           key1
-      </button>
+      </ToggleButton>
 
-      <button
+      <ToggleButton
         onClick={() => updateKeys("key2")}>
           key2
-      </button>
+      </ToggleButton>
 
-      <button
+      <ToggleButton
         onClick={() => updateKeys("key3")}>
           key3
-      </button>
+      </ToggleButton>
+    </ToggleButtonGroup>
 
       \
-      <button
+      <ToggleButton
+        value="check"
+        selected={selected}
+        onChange={() => {
+          setSelected(!selected);
+        }}
         onClick={() => toggleGeolocation()}>
           geolocation
-      </button>
+      </ToggleButton>
       /
     </section>
   )
